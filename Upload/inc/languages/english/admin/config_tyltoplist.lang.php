1de6b47fe61e2d25d45cbae243d8c921ec65c918
Sven <SvePu@users.noreply.github.com>
2022-01-28 22:31:53 +0100

    Fixes #11

--- Upload/inc/languages/english/admin/config_tyltoplist.lang.php
+++ Upload/inc/languages/english/admin/config_tyltoplist.lang.php
@@ -3,7 +3,7 @@
 	Language admin file for 'TopList AddOn für THX/Like' plugin for MyBB 1.8
 	Language: english
 	Copyright © 2015 Svepu
-	Last change: 2022-01-26
+	Last change: 2022-01-28
 */
 
 $l['tyltoplist'] = 'TopList addon for Thank You/Like System';
@@ -12,19 +12,19 @@ $l['mainplugin_req'] = 'The required <a href="https://community.mybb.com/mods.ph
 $l['update_mainplugin_req'] = 'Please update the <a href="https://community.mybb.com/mods.php?action=view&pid=360" target="_blank">ThankYou/Like Plugin</a> to a version higher/equal v.3.0.0!';
 
 $l['setting_group_tyltoplist'] = 'TYL-TopList Settings';
-$l['setting_group_tyltoplist_desc'] = 'Setting of the TYL-TopList plugin';
-$l['setting_tyltoplist_enable'] = 'Do you want enabe this feature?';
-$l['setting_tyltoplist_enable_desc'] = 'Set YES to enable it!';
-$l['setting_tyltoplist_limit'] = 'How many lines you want show?';
-$l['setting_tyltoplist_limit_desc'] = 'Set the list limit! (default:20)';
-$l['setting_tyltoplist_usernames'] = 'Show usernames in user group based style?';
-$l['setting_tyltoplist_usernames_desc'] = 'Choose YES to enable this feature!';
-$l['setting_tyltoplist_group'] = 'Usergroups who can view the toplist.';
-$l['setting_tyltoplist_group_desc'] = 'Select usergroups who can view the toplist.';
-$l['setting_tyltoplist_fids'] = 'Excluding thanked/liked posts from certain forums. (eg: OffTopic)';
-$l['setting_tyltoplist_fids_desc'] = 'Select to exclude forums. (Info: only Forums - no categories - default: none)';
+$l['setting_group_tyltoplist_desc'] = 'Setting of TYL-TopList plugin';
+$l['setting_tyltoplist_enable'] = 'Enable TYL-TopList';
+$l['setting_tyltoplist_enable_desc'] = 'Do you want enable this feature? - Set YES to turn it on!';
+$l['setting_tyltoplist_limit'] = 'Top List Limits';
+$l['setting_tyltoplist_limit_desc'] = 'How many lines you want show? - Set the list limit! (default:20)';
+$l['setting_tyltoplist_usernames'] = 'Username Group Style';
+$l['setting_tyltoplist_usernames_desc'] = 'Choose YES to show usernames in user group based style!';
+$l['setting_tyltoplist_group'] = 'Visible for Groups';
+$l['setting_tyltoplist_group_desc'] = 'Select user groups that can see the TYL-TopList.';
+$l['setting_tyltoplist_fids'] = 'Exclude Forums from TYL-TopList';
+$l['setting_tyltoplist_fids_desc'] = 'Select forums to exclude (eg: OffTopic) - (Info: only Forums - no categories - default: none)';
 $l['setting_tyltoplist_show'] = 'Location of TYL-Toplist';
 $l['setting_tyltoplist_show_desc'] = 'Choose the place where you want show the TYL-Toplist.';
-$l['setting_tyltoplist_show_1'] = 'On a separate page (/tyltoplist.php)';
+$l['setting_tyltoplist_show_1'] = 'On separate page (/tyltoplist.php)';
 $l['setting_tyltoplist_show_2'] = 'In board statistics on index page';
-$l['setting_tyltoplist_show_3'] = 'In extended forum statistics page (/stats.php)';
+$l['setting_tyltoplist_show_3'] = 'In forum statistics page (/stats.php)';
